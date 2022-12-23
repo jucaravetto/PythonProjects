206416c6aff806e16bf4f49c5ffbd3cf77fc319f
Julianna Caravetto <julianna.caravetto@gmail.com>
2022-12-23 11:23:26 -0300

    Starting with the sum part

--- Calc.py
+++ Calc.py
@@ -1,1 +1,7 @@
-print("Hello World!!")
+print("Saudações! Esta é sua calculadora feita em Python!\n")
+
+num1 = int(input('Entre com seu primeiro número: '))
+num2 = int(input('Entre com seu segundo número: '))
+
+print("\nA soma é:")
+print(num1+num2,"\n")
