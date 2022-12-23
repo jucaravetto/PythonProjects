1541574e3ed8b691557d87602efee34d109b68b7
Julianna Caravetto <julianna.caravetto@gmail.com>
2022-12-23 11:38:16 -0300

    Adding other math operators

--- Calc.py
+++ Calc.py
@@ -3,5 +3,27 @@ print("Saudações! Esta é sua calculadora feita em Python!\n")
 num1 = int(input('Entre com seu primeiro número: '))
 num2 = int(input('Entre com seu segundo número: '))
 
-print("\nA soma é:")
-print(num1+num2,"\n")
+#teste comentário
+#to usando o seguinte site pra aprender:
+#https://www.digitalocean.com/community/tutorials/how-to-make-a-calculator-program-in-python-3
+
+#Primeiro jeito que fiz, agora testar nova formatação
+#print("\nA soma é:")   
+#print(num1+num2,"\n")
+
+# Addition
+#print("Testar nova exibição aqui")
+print('{} + {} = '.format(num1, num2),num1 + num2) #exibe o resultado logo depois de calcular a soma
+#print(num1 + num2)
+
+# Subtraction
+print('{} - {} = '.format(num1, num2),num1 - num2)
+#print(num1 - num2)
+
+# Multiplication
+print('{} * {} = '.format(num1, num2),num1 * num2)
+#print(num1 * num2)
+
+# Division
+print('{} / {} = '.format(num1, num2),num1 / num2)
+#print(num1 / num2)
